e3f88c03b4547999ce628cfc79ebbab9349babf7
André Elias <dedekpo@gmail.com>
2023-08-24 15:12:32 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const axios = require("axios");
+const express = require("express");
+const app = express();
+const port = 3000;
+const cors = require("cors");
+
+app.use(cors());
+
+app.get("/universities/:country", async (req, res) => {
+	try {
+		const country = req.params.country;
+
+		const universities = await axios.get(
+			`http://universities.hipolabs.com/search?country=${country}`
+		);
+
+		const filteredData = universities.data.slice(0, 50);
+
+		res.json(filteredData);
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`);
+});
